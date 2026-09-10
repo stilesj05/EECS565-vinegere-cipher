096e65941334996fd536af15813880b7e2226cac
Jaydine Stiles <stilesjaydine@gmail.com>
2026-09-10 17:55:31 -0500

    Initial project setup

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //allows us to use input and output
+
+using namespace std; //allows us to use standard c++ names
+
+int main() { //main function where the program starts
+    cout << "Vigenere Cipher" << endl; //prints a test message
+
+    return 0; //ends the program successfully
+}
